069e63ffb626cc9c52b1d89844039d57a5d6bbc2
shuvra442 <shuvrapatra5@gmail.com>
2023-12-23 01:54:28 +0530

    connect mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express();
-app.listen(3000, ()=>{
-  console.log("Server is running on port 3000!!");
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
