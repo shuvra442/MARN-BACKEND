32b910821fe1559c2f7dd8122188c13f297f5ce2
shuvra442 <shuvrapatra5@gmail.com>
2023-12-27 11:15:12 +0530

    create and add OAuth

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     },
+  avatar: {
+    type:String,
+    default: "https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg" 
+  },
 }, {timestamps: true});
 
 const User=mongoose.model('User', userSchema);
